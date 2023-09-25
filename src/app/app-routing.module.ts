788d7fc0ee2e9a3596ae7bce46e072202c9cb839
Sneha Hande <sneha.hande@atos.net>
2023-09-25 18:15:19 +0530

    save

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,7 @@ import { LoginComponent } from './login/login.component';
 import { ErrorComponent } from './error/error.component';
 
 const routes: Routes = [
-  // { path: '', redirectTo: '/grid-page', pathMatch: 'full' },
+  { path: '', redirectTo: '/grid-page', pathMatch: 'full' },
   // { path: 'login', component: LoginComponent },
   { path: 'grid-page', component: LandingPageComponent },
   { path: 'case_study/:id', component: DetailsPageComponent },
